05bb4519f4ce15ddaf1ca6611994acd822cfa55b
elichtenberg93 <erl15@duke.edu>
2014-12-15 07:13:33 -0500

    Properties file fixed

--- src/data/DataManagerFactory.java
+++ src/data/DataManagerFactory.java
@@ -3,21 +3,24 @@
 
 package data;
 
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class DataManagerFactory {
 	
 	public static final String DATA_RESOURCE_PACKAGE = "data/";
-	public static final String DATA_RESOURCE_NAME = "data_configurations.properties";
+	public static final String DATA_RESOURCE_NAME = "dataConfigurations";
 	
 	private ResourceBundle myDataConfigs;
 	
 	public DataManagerFactory() {
+		Locale loc = new Locale("en", "US");
 		myDataConfigs = ResourceBundle.getBundle(DATA_RESOURCE_PACKAGE + DATA_RESOURCE_NAME);
 	}
 	
 	public DataManager create() {
 		String dataType = myDataConfigs.getString("Type");
+		//String dataType = "JSON";
 		return constructDataManager(dataType);
 	}
 
